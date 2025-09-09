4254a2c2f3272a53c11919066b7d46e0fcce6139
yacine2003 <113189481+yacine2003@users.noreply.github.com>
2025-09-09 14:05:07 +0200

    Initialisation du serveur

---
+++ quiz-api/app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route('/')
+def hello_world():
+	x = 'world'
+	return f"Hello, {x}"
+
+if __name__ == "__main__":
+    app.run()
